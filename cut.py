7a970090e189b2f74af3cc5ca3e30fc9a6cc481e
Brandon Mechtley <bmechtley@gmail.com>
2012-12-18 14:44:19 -0700

    no significant change

--- cut.py
+++ cut.py
@@ -5,7 +5,7 @@ natural-mixer
 2012 Brandon Mechtley
 Arizona State University
 
-Uses information embedded in Sonic Visualizer export file to remove segments on a layer called
+Uses information embedded in Sonic Visualizer export file to remove segments on a layer called 
 "cut", crossfading the adjacent sections with an equal-power logarithmic crossfade with length
 specified by the second command-line argument, in millesconds.
 
