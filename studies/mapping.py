bac3fa511f625d61d860182a0acda83dd2ca8d86
Brandon Mechtley <bmechtley@gmail.com>
2013-03-05 17:06:45 -0700

    various inspection fixes

--- studies/mapping.py
+++ studies/mapping.py
@@ -1,17 +1,16 @@
-'''
+"""
 mturk/mapping.py
 envmixer
 
 2013 Brandon Mechtley
 Arizona State University
 
-This script moves files based on mapping.txt. See makemapping.sh for more
-information.
+This script moves files based on mapping.txt. See makemapping.sh for more information.
 
 Usage: python mapping.py {do,undo}
     do: move original wavfiles to [md5].wav files.
     undo: move the [md5].wav files to their original names.
-'''
+"""
 
 import sys
 import subprocess
@@ -19,6 +18,10 @@ import subprocess
 f = open('mapping.txt', 'rb')
 
 def undomapping():
+    """
+    Move file hashes back to their original filenames.
+    """
+    
     for line in f:
         s = line.rstrip('\n').split(', ')
         print 'mv', s[1], s[0]
@@ -27,6 +30,10 @@ def undomapping():
     exit()
 
 def domapping():
+    """
+    Move all mapped files to their hash.
+    """
+    
     for line in f:
         s = line.rstrip('\n').split(', ')
         print 'mv', s[0], s[1]
